ac14d7577508afd369059620901602302b0f3e4a
Len Budney <Len.Budney@grantstreet.com>
2019-01-04 16:16:42 -0500

    cleanup of usda init

--- init-usda-db.sql
+++ init-usda-db.sql
@@ -1,8 +1,5 @@
-# Prepend for mysqldump output to create DB from scratch
+-- Prepend for mysqldump output to create DB from scratch
 DROP DATABASE IF EXISTS usda;
 CREATE DATABASE usda CHARSET utf8 COLLATE utf8_general_ci;
 USE usda;
-
-
-
 
